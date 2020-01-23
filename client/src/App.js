1a6ecfb41c34c26181342d38807a17c731dc506a
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-23 10:54:55 +0100

    add sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 import io from "socket.io-client";
 const socket = io();
