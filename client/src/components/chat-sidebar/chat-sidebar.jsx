7c90c23be7dd97bd4a5b08c37f12b7afb57331b8
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-23 16:14:09 +0100

    add structure chat-sidebar

--- client/src/components/chat-sidebar/chat-sidebar.jsx
+++ client/src/components/chat-sidebar/chat-sidebar.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
+import List from "./chat-widget-list";
+
 export default () => {
   return (
     <div>
       {/* User status */}
+      <div className="Widget-User">
+        <div className="status">
+          <span className="active"></span>
+        </div>
+        <div className="info">
+          <p className="name">Alessandro Aloisio</p>
+          <p className="description">FullStack Web Developer</p>
+        </div>
+      </div>
       {/* Search */}
+      <div className="Widget-Search">
+        <div className="input-search">
+          <input type="text" />
+          <button>
+            <i className="flaticon-loupe"></i>
+          </button>
+        </div>
+      </div>
       {/* Groupes */}
+      <List name="Groupes" />
       {/* Users */}
+      <List name="Users" />
     </div>
   );
 };
---
+++ client/src/components/chat-sidebar/chat-widget-list.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default props => {
+  return (
+    <div className="Widget-List">
+      <div className="widget-title">{props.name}</div>
+    </div>
+  );
+};
