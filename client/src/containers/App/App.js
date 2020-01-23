535ea481e656f03b6230d65b2fcda6cb819bc9cc
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-23 14:06:06 +0100

    add chat container

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../../components/header/header";
+import Chat from "../Chat/Chat";
 
 import "../../assets/icons/flaticon.css";
 import "./App.scss";
@@ -9,6 +10,8 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+
+      <Chat />
     </div>
   );
 };
---
+++ client/src/containers/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Chat.scss";
+
+const Chat = () => {
+  return (
+    <div className="Chat">
+      <h2>Sidebar</h2>
+      <h2>Content</h2>
+    </div>
+  );
+};
+
+export default Chat;
