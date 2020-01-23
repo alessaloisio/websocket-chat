d20cf3d05b88a1a13c5621b13d0841bd3eb10a52
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-23 09:40:03 +0100

    fork from websocket-react

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ import socket from "socket.io";
 import path from "path";
 
 const PORT = process.env.PORT || 3000;
-const STATIC_DIR = path.join(__dirname, "../", "client", "build");
 
 const app = express();
 const server = http.Server(app);
 const io = socket(server);
 
+// NODE SEND THE FRONT-END
+const STATIC_DIR = path.join(__dirname, "../", "client", "build");
 app.use(express.static(STATIC_DIR));
+
+// Start the NODE SERVER
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+// Handler where a client are connected
 io.on("connection", client => {
   console.log(`Client ${client.id} connected`);
   client.on("disconnect", () => console.log("Client disconnected"));
