f119cd32b9647a9877393dc4c325a00816239787
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-26 11:53:13 +0100

    clean server emit

--- src/server.js
+++ src/server.js
@@ -21,20 +21,3 @@ io.on("connection", client => {
   console.log(`Client ${client.id} connected`);
   client.on("disconnect", () => console.log("Client disconnected"));
 });
-
-setInterval(() => {
-  const time = new Date();
-
-  const twoDigit = str => {
-    str = "00" + str;
-    str = str.substring(str.length - 2);
-    return str;
-  };
-
-  io.emit(
-    "updateTime",
-    `${twoDigit(time.getHours())} :
-    ${twoDigit(time.getMinutes())} :
-    ${twoDigit(time.getSeconds())}`
-  );
-}, 1000);
