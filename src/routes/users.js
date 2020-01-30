f2a9897f51a5c921f536331ca27ee3513580a697
Alessandro Aloisio <alessaloisio@gmail.com>
2020-01-30 15:43:57 +0100

    get user logged info, specific user and search by name

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,43 @@
 import { Router } from "express";
 
+import User from "./../models/user";
+
 const router = new Router();
 
-router.use("/", (req, res) => {
+router.use("/search/:name", async (req, res) => {
+  const name = req.params.name || "";
+
+  const user = await User.find({
+    "info.name": {
+      $regex: `.*${name}.*`,
+      $options: "i"
+    }
+  });
+
   res.json({
-    me: "me"
+    data: user
+  });
+});
+
+// https://expressjs.com/fr/guide/routing.html
+router.use("/:login", async (req, res) => {
+  const login = req.params.login || "";
+
+  const user = await User.findOne({
+    login: {
+      $eq: login
+    }
+  });
+
+  res.json({
+    data: user
+  });
+});
+
+router.use("/", async (req, res) => {
+  const user = await User.findById(req.user.id);
+  res.json({
+    data: user
   });
 });
 
