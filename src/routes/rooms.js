7a716510dd73d4ec4bedc163facbc165ab1212f7
Alessandro Aloisio <alessaloisio@gmail.com>
2020-02-07 11:22:07 +0100

    refactor rooms route, fn getRoomInfo

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -7,79 +7,6 @@ import Group from "../models/group";
 
 const router = new Router();
 
-const lookupUsersRoom = userId => ({
-  $lookup: {
-    from: "users",
-    let: { uid: "$users" },
-    pipeline: [
-      {
-        $match: {
-          $expr: {
-            $and: [{ $ne: ["$_id", userId] }, { $in: ["$_id", "$$uid"] }]
-          }
-        }
-      }
-    ],
-    as: "users_info"
-  }
-});
-
-/**
- * Get/Create a room for user who want to join a room
- * Need to return a room informations for the user
- */
-router.get("/search/:id", async (req, res) => {
-  let type, roomId;
-  let dest = req.params.id || null;
-
-  // detect user or group => Number or ObjectId
-  if (dest == parseInt(dest)) {
-    type = "user";
-    dest = parseInt(dest);
-    roomId = dest * req.user.id; // Number
-  } else {
-    type = "group";
-    roomId = dest; // ObjectID
-  }
-
-  const findRoom = await Room.findById(roomId);
-
-  // User search an another user and want to create a room to chat with him
-  if (!findRoom && type === "user") {
-    const room = new Room();
-    room._id = roomId;
-    room.users = [req.user.id, dest];
-    await room.save();
-  }
-
-  // User want to join a group
-  if (findRoom && type === "group") {
-    if (!findRoom.users.includes(req.user.id)) {
-      findRoom.users.push(req.user.id);
-      await findRoom.save();
-    }
-  }
-
-  /***
-   * GET INFOS
-   */
-  const users_info = await User.find({ _id: findRoom.users });
-  // Verify if user put the room on favourite
-  const favourite = !!(await Favourite.findOne({
-    room: roomId,
-    user: req.user.id
-  }));
-
-  let group = {};
-  if (roomId != parseInt(roomId)) {
-    group = await Group.findById(roomId);
-  }
-
-  res.json({
-    data: { ...findRoom._doc, favourite, users_info, group }
-  });
-});
-
 // Create group
 router.post("/groups/", (req, res) => {
   // Create the new document
@@ -130,7 +57,6 @@ router.get("/favourites/:id", (req, res) => {
           const fav = new Favourite();
           fav.user = req.user.id;
           fav.room = roomId;
-
           return fav.save();
         }
 
@@ -156,6 +82,64 @@ router.get("/favourites/:id", (req, res) => {
     });
 });
 
+// INFOS ROOM
+const getInfoRoom = async (user, room) => {
+  const users_info = await User.find({ _id: room.users });
+
+  const favourite = !!(await Favourite.findOne({
+    room: room._id,
+    user
+  }));
+
+  const group =
+    room._id != parseInt(room._id) ? await Group.findById(room._id) : null;
+
+  return { favourite, users_info, group };
+};
+
+/**
+ * Get/Create a room for user who want to join a room
+ * Need to return a room informations for the user
+ */
+router.get("/search/:id", async (req, res) => {
+  let type, roomId;
+  let dest = req.params.id || null;
+
+  // detect user or group => Number or ObjectId
+  if (dest == parseInt(dest)) {
+    type = "user";
+    dest = parseInt(dest);
+    roomId = dest * req.user.id; // Number
+  } else {
+    type = "group";
+    roomId = dest; // ObjectID
+  }
+
+  const room = await Room.findById(roomId);
+
+  // User search an another user and want to create a room to chat with him
+  if (!room && type === "user") {
+    const newRoom = new Room();
+    newRoom._id = roomId;
+    newRoom.users = [req.user.id, dest];
+    await newRoom.save();
+  }
+
+  // User want to join a group
+  if (room && type === "group") {
+    if (!room.users.includes(req.user.id)) {
+      room.users.push(req.user.id);
+      await room.save();
+    }
+  }
+
+  const infos = await getInfoRoom(req.user.id, room);
+
+  res.json({
+    data: { ...room._doc, ...infos }
+  });
+});
+
 // Get a room
 router.get("/:id", async (req, res) => {
   const roomId = req.params.id || null;
@@ -166,21 +150,11 @@ router.get("/:id", async (req, res) => {
       users: { $eq: req.user.id } //
     });
 
-    const users_info = await User.find({ _id: room.users });
-
-    const favourite = !!(await Favourite.findOne({
-      room: roomId,
-      user: req.user.id
-    }));
-
-    const group =
-      roomId != parseInt(roomId) ? await Group.findById(roomId) : null;
+    const infos = await getInfoRoom(req.user.id, room);
 
     res.json({
       ...room._doc,
-      users_info,
-      favourite,
-      group
+      ...infos
     });
   } else
     res.json({
@@ -188,18 +162,13 @@ router.get("/:id", async (req, res) => {
     });
 });
 
-// Get all infos
+// Get all user rooms
 router.get("/", async (req, res) => {
   const userRooms = {
     favourites: [],
     peoples: [],
     groups: []
   };
-
-  const favourites = Array.from(
-    await Favourite.find({ user: req.user.id }, { room: 1, _id: 0 }),
-    ({ room }) => room
-  );
 
   const rooms = await Promise.all(
     (
@@ -212,26 +181,19 @@ router.get("/", async (req, res) => {
         }
       )
     ).map(async room => {
-      const newObj = {
-        ...room._doc
+      const infos = await getInfoRoom(req.user.id, room);
+
+      return {
+        ...room._doc,
+        ...infos
       };
-
-      newObj.users_info = await User.find({ _id: room.users });
-
-      if (room._id != parseInt(room._id))
-        newObj.group = await Group.findById(room._id);
-
-      return newObj;
     })
   );
 
   rooms.map(room => {
-    if (favourites.includes(room._id)) userRooms.favourites.push(room);
-    else if (
-      parseInt(room._id) === room.users.reduce((acc, curr) => acc * curr)
-    )
-      userRooms.peoples.push(room);
-    else userRooms.groups.push(room);
+    if (room.favourite) userRooms.favourites.push(room);
+    else if (room.group) userRooms.groups.push(room);
+    else userRooms.peoples.push(room);
   });
 
   res.json({ ...userRooms });
